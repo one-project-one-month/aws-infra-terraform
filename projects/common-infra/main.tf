a1c04e2ff1cd91021a2422990bb58973a9c703da
Hein Htet Win <heinhtetwin386@gmail.com>
2025-07-24 01:20:09 +0700

    comment cms for now

--- projects/common-infra/main.tf
+++ projects/common-infra/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0"
-    }
-  }
-  required_version = ">= 1.12.0"
-}
-
 provider "aws" {
   region = local.region
 }
